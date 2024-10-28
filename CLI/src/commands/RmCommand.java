bfb4cdc9f1be771ab7da089cfd5edc539db4e297
elkholyjr <119688248+elkholyjr@users.noreply.github.com>
2024-10-28 19:17:15 +0300

    Update RmCommand.java

--- CLI/src/commands/RmCommand.java
+++ CLI/src/commands/RmCommand.java
@@ -4,5 +4,15 @@ import java.io.File;
 
 public class RmCommand {
     public void execute(String[] args) {
+        if(args.length<1){
+            System.out.println("Error: Missing file name.");
+            return;
+        }
+        File file=new File(args[0]);
+        if(file.isFile()&&file.delete()){
+            System.out.println("File removed: " + args[0]);
+        }else{
+            System.out.println("Failed to remove file: " + args[0] + " (Make sure it is empty)");
+        }
     }
 }
