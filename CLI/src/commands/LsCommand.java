c00daecc8cf57d9b83fa88cc3df25f3ee5df6b34
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 01:04:03 +0300

    Ls Command Done working on the ls command

--- CLI/src/commands/LsCommand.java
+++ CLI/src/commands/LsCommand.java
@@ -1,6 +1,24 @@
 package commands;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class LsCommand {
     public void execute(String[] args) {
+        String directoryPath = "."; //Needs to be changed to match current working directory
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isHidden())
+                    continue;
+                else
+                    System.out.println(file.getName());
+            }
+        }
     }
 }
