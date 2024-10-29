2684f7dad66a25f5b2f2296f6b7cc896792c95d2
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 01:10:09 +0300

    LsA command Done working on ls -a command

--- CLI/src/commands/LsACommand.java
+++ CLI/src/commands/LsACommand.java
@@ -1,6 +1,17 @@
 package commands;
 
+import java.io.File;
+
 public class LsACommand {
     public void execute(String[] args) {
+        String directoryPath = "."; //Needs to be changed to match current working directory
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
     }
 }
