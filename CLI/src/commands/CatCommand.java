713991f565de2c75833a7193032f64ffdb7267ec
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 18:45:55 +0300

    Added a terminator for the echoing feature

--- CLI/src/commands/CatCommand.java
+++ CLI/src/commands/CatCommand.java
@@ -9,18 +9,22 @@ import java.util.Scanner;
 public class CatCommand {
     public void execute(File currentDirectory, String[] args) {
         if (args.length < 1) {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            System.out.println(input);
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                System.out.println(input);
+                if (input.equals("]")) {
+                    break;
+                }
+            }
             return;
         }//reading
         String content = "";
         File file = new File(args[0]);
-        try{
+        try {
             content = Files.readString(Paths.get(args[0])).replace("\n", " ").replace("\r", " ");
             System.out.println(content);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
     }
