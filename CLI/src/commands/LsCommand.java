9575c0005ad9eb5a27bca6333659ead84cfc0dad
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 15:11:36 +0300

    Added currentDirectory

--- CLI/src/commands/LsCommand.java
+++ CLI/src/commands/LsCommand.java
@@ -1,6 +1,24 @@
 package commands;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class LsCommand {
-    public void execute(String[] args) {
+    public void execute(File currentDirectory, String[] args) {
+//        String directoryPath = "."; //Needs to be changed to match current working directory
+        File directory = new File(String.valueOf(currentDirectory));
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isHidden())
+                    continue;
+                else
+                    System.out.println(file.getName());
+            }
+        }
     }
 }
