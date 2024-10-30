8bf322bef6e239b734f6d737ad5fd282a4208832
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 15:11:05 +0300

    Added currentDirectory

--- CLI/src/commands/LsACommand.java
+++ CLI/src/commands/LsACommand.java
@@ -1,6 +1,17 @@
 package commands;
 
+import java.io.File;
+
 public class LsACommand {
-    public void execute(String[] args) {
+    public void execute(File currentDirectory, String[] args) {
+//        String directoryPath = "."; //Needs to be changed to match current working directory
+        File directory = new File(String.valueOf(currentDirectory));
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
     }
 }
