f32399f7c1164c03514e23b40753e2dd6e408bc9
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 15:27:15 +0300

    worked on the ls -r command

--- CLI/src/commands/LsRCommand.java
+++ CLI/src/commands/LsRCommand.java
@@ -1,6 +1,19 @@
 package commands;
 
+import java.io.File;
+
 public class LsRCommand {
-    public void execute(String[] args) {
+    public void execute(File currentDirectory, String[] args) {
+        File directory = new File(String.valueOf(currentDirectory));
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (int i = files.length - 1; i >= 0; i--) {
+                if (files[i].isHidden()) {
+                    continue;
+                } else
+                    System.out.println(files[i].getName());
+            }
+        }
     }
 }
