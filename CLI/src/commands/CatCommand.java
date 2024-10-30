e0700704c9f46f1da582e1390ef0b02206922f4e
ShaherSaaed <shahersa3ed004@gmail.com>
2024-10-30 15:08:37 +0300

    Added currentDirectory to .java file

--- CLI/src/commands/CatCommand.java
+++ CLI/src/commands/CatCommand.java
@@ -1,6 +1,24 @@
 package commands;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CatCommand {
-    public void execute(String[] args) {
+    public void execute(File currentDirectory, String[] args) {
+        if (args.length < 1) {
+            System.out.println("Error: Missing file name.");
+            return;
+        }//reading
+        String content = "";
+        File file = new File(args[0]);
+        try{
+            content = Files.readString(Paths.get(args[0])).replace("\n", " ").replace("\r", " ");
+            System.out.println(content);
+        }
+        catch (IOException e){
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
     }
 }
